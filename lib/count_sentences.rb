2d7e30adae7903e6084b4ea3d8be962e05e4ad32
Filip Todoroski <ftodoroski@ymail.com>
2020-01-09 16:55:04 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    return 0 if self == ""
 
+    self.split(" ").count { |ele| ele.sentence? || ele.question? || ele.exclamation? }
   end
 end
